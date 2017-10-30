c2c3b434d34c625a864291ea1a014c43ccd58865
Philip Basford <pjb@ecs.soton.ac.uk>
2017-10-30 11:49:44 +0000

    basic pi current coversion added

--- conversions.py
+++ conversions.py
@@ -10,6 +10,9 @@ V5_R2 = 3900
 PI_R1 = 2000
 PI_R2 = 3000
 
+PI_C_SENSE = 0.05
+PI_C_R_OUT = 3300
+
 def convert_vin(reading):
     return _convert_adc(reading, VIN_R1, VIN_R2)
 
@@ -23,11 +26,17 @@ def convert_pi_v(reading):
     return _convert_adc(reading, PI_R1, PI_R2)
 
 def convert_pi_c(reading):
-    pass
+    return (100 * _adc_to_v(reading))/(PI_C_SENSE * PI_C_R_OUT)
 
 def _convert_adc(adc, res1, res2):
     """
         R1 = the upper of the pair
         R2 = the lower of the pair
     """
-    return (adc * VREF /ADC_MAX) * (res1 + res2) / res2
+    return _adc_to_v(adc) * (res1 + res2) / res2
+
+def _adc_to_v(adc):
+    """
+        Convert an ADC reading the voltage being put into it
+    """
+    return adc * VREF / ADC_MAX
